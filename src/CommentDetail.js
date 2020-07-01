0f95bfb5eed7d66a0b014a13204f909eff7a7300
itsjives <jonathanwcapra@gmail.com>
2020-06-30 18:33:14 -0700

    loading props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = props => {
-    console.log(props);
     return (
         <div className="comment">
                 <a href="/" className="avatar">
-                    <img src={faker.image.avatar()}/>
+                    <img src={props.avatar} alt="image"/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
                         {props.author}
                     </a>
                     <div className="metadata">
-                        <span className="date">Today at 6pm</span>
+                        <span className="date">{props.timeAgo}</span>
                     </div>
-                    <div className="text">Nice Post!</div>
+                    <div className="text">{props.post}</div>
                 </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="sam"/>
-            <CommentDetail author="jordan"/>
-            <CommentDetail author="katie"/>
+            <CommentDetail 
+                author="sam" 
+                timeAgo="Today at 6:00pm" 
+                post="Yoo"
+                avatar={faker.image.avatar()}/>
+            <CommentDetail 
+                author="jordan" 
+                timeAgo="Yesterday at 10:00am" 
+                post="nice drop"
+                avatar={faker.image.avatar()}/>
+            <CommentDetail 
+                author="katie" 
+                timeAgo="about a minute ago" 
+                post="farty magoo"
+                avatar={faker.image.avatar()}/>
         </div>
     );
 };
